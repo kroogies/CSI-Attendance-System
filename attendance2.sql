4a33bb73331010aecc626fe8512165293e0b3da5
kroogies <104058809+kroogies@users.noreply.github.com>
2023-06-09 20:17:24 +0800

    Add files via upload

---
+++ attendance2.sql
@@ -0,0 +1,277 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: attendance
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `id_0007606744`
+--
+
+DROP TABLE IF EXISTS `id_0007606744`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `id_0007606744` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` varchar(255) DEFAULT NULL,
+  `time_in` varchar(255) DEFAULT NULL,
+  `time_out` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `id_0007606744`
+--
+
+LOCK TABLES `id_0007606744` WRITE;
+/*!40000 ALTER TABLE `id_0007606744` DISABLE KEYS */;
+/*!40000 ALTER TABLE `id_0007606744` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `id_0008264524`
+--
+
+DROP TABLE IF EXISTS `id_0008264524`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `id_0008264524` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` varchar(255) DEFAULT NULL,
+  `time_in` varchar(255) DEFAULT NULL,
+  `time_out` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `id_0008264524`
+--
+
+LOCK TABLES `id_0008264524` WRITE;
+/*!40000 ALTER TABLE `id_0008264524` DISABLE KEYS */;
+/*!40000 ALTER TABLE `id_0008264524` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `id_0008385348`
+--
+
+DROP TABLE IF EXISTS `id_0008385348`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `id_0008385348` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` varchar(255) DEFAULT NULL,
+  `time_in` varchar(255) DEFAULT NULL,
+  `time_out` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `id_0008385348`
+--
+
+LOCK TABLES `id_0008385348` WRITE;
+/*!40000 ALTER TABLE `id_0008385348` DISABLE KEYS */;
+/*!40000 ALTER TABLE `id_0008385348` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `id_0008407268`
+--
+
+DROP TABLE IF EXISTS `id_0008407268`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `id_0008407268` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` varchar(255) DEFAULT NULL,
+  `time_in` varchar(255) DEFAULT NULL,
+  `time_out` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `id_0008407268`
+--
+
+LOCK TABLES `id_0008407268` WRITE;
+/*!40000 ALTER TABLE `id_0008407268` DISABLE KEYS */;
+/*!40000 ALTER TABLE `id_0008407268` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `id_0008531832`
+--
+
+DROP TABLE IF EXISTS `id_0008531832`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `id_0008531832` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` varchar(255) DEFAULT NULL,
+  `time_in` varchar(255) DEFAULT NULL,
+  `time_out` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `id_0008531832`
+--
+
+LOCK TABLES `id_0008531832` WRITE;
+/*!40000 ALTER TABLE `id_0008531832` DISABLE KEYS */;
+/*!40000 ALTER TABLE `id_0008531832` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `id_0008532623`
+--
+
+DROP TABLE IF EXISTS `id_0008532623`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `id_0008532623` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` varchar(255) DEFAULT NULL,
+  `time_in` varchar(255) DEFAULT NULL,
+  `time_out` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `id_0008532623`
+--
+
+LOCK TABLES `id_0008532623` WRITE;
+/*!40000 ALTER TABLE `id_0008532623` DISABLE KEYS */;
+/*!40000 ALTER TABLE `id_0008532623` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `id_0008717702`
+--
+
+DROP TABLE IF EXISTS `id_0008717702`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `id_0008717702` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` varchar(255) DEFAULT NULL,
+  `time_in` varchar(255) DEFAULT NULL,
+  `time_out` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `id_0008717702`
+--
+
+LOCK TABLES `id_0008717702` WRITE;
+/*!40000 ALTER TABLE `id_0008717702` DISABLE KEYS */;
+/*!40000 ALTER TABLE `id_0008717702` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `id_0008721967`
+--
+
+DROP TABLE IF EXISTS `id_0008721967`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `id_0008721967` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` varchar(255) DEFAULT NULL,
+  `time_in` varchar(255) DEFAULT NULL,
+  `time_out` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `id_0008721967`
+--
+
+LOCK TABLES `id_0008721967` WRITE;
+/*!40000 ALTER TABLE `id_0008721967` DISABLE KEYS */;
+/*!40000 ALTER TABLE `id_0008721967` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `id_0008899484`
+--
+
+DROP TABLE IF EXISTS `id_0008899484`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `id_0008899484` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` varchar(255) DEFAULT NULL,
+  `time_in` varchar(255) DEFAULT NULL,
+  `time_out` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `id_0008899484`
+--
+
+LOCK TABLES `id_0008899484` WRITE;
+/*!40000 ALTER TABLE `id_0008899484` DISABLE KEYS */;
+/*!40000 ALTER TABLE `id_0008899484` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `id_0009041509`
+--
+
+DROP TABLE IF EXISTS `id_0009041509`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `id_0009041509` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` varchar(255) DEFAULT NULL,
+  `time_in` varchar(255) DEFAULT NULL,
+  `time_out` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `id_0009041509`
+--
+
+LOCK TABLES `id_0009041509` WRITE;
+/*!40000 ALTER TABLE `id_0009041509` DISABLE KEYS */;
+/*!40000 ALTER TABLE `id_0009041509` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-09 20:13:52
